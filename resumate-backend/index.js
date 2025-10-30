a945300b5ca957f7c7a66390fcee3f54428fd7a5
Rishabh5911 <rishabhpokhriyal259@gmail.com>
2025-10-30 16:06:52 +0530

    code update

--- resumate-backend/index.js
+++ resumate-backend/index.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN, 
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
